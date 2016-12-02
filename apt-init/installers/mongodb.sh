e2f5af2b8b14cd364243e08ec05a6b4685859dc6
Alexander Martin <alex@suitupalex.com>
2016-12-02 15:49:07 -0600

    mongo: Updated to 3.4 and fixed init.d script.

--- apt-init/installers/mongodb.sh
+++ apt-init/installers/mongodb.sh
@@ -5,13 +5,13 @@
 log "Adding MongoDB APT Key:" && \
 apt-key adv \
   --keyserver hkp://keyserver.ubuntu.com:80 \
-  --recv EA312927 \
+  --recv 0C49F3730359A14518585931BC711F9BA15703C6 \
 && \
 
 log "Adding MongoDB APT Soure:" && \
 echo \
-  "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" \
-  | tee /etc/apt/sources.list.d/mongodb-org-3.2.list \
+  "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" \
+  | tee /etc/apt/sources.list.d/mongodb-org-3.4.list \
 && \
 
 log "Updating APT:" && \
@@ -32,8 +32,11 @@ apt-get install \
   mongodb-org \
 && \
 
-log "Copying mongod.service:" && \
-cp mongod.service /lib/systemd/system/mongod.service && \
+log "Adding init.d service script:" && \
+wget https://raw.githubusercontent.com/mongodb/mongo/master/debian/init.d \
+  -O /etc/init.d/mongod \
+&& \
+chmod 755 /etc/init.d/mongod && \
 
 log "Starting mongod service:" && \
 service mongod start
