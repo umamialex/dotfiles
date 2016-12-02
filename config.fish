4144544fb3e23b58ccdecc33a742b3c03d182094
Alexander Martin <alex@suitupalex.com>
2016-12-02 15:57:44 -0600

    fish: Added ctrl and agg aliases.

--- config.fish
+++ config.fish
@@ -3,6 +3,8 @@ alias ls="ls -laX --color"
 alias dev="cd ~/Development"
 alias me="dev; and cd martin-experiments"
 alias p="dev; and cd personal"
+alias ctrl="me; and cd ctrl"
+alias agg="me; and cd athletes.gg"
 
 setenv SSH_ENV $HOME/.ssh/environment
 set PATH ~/.npm-global/bin $PATH
