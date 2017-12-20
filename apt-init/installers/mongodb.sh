411cc32f6b992bf37ae7f0bdd8ee55877258e7df
Alexander Martin <alex@suitupalex.com>
2017-12-20 10:47:22 -0600

    mongodb: Upgrade to 3.6.

--- apt-init/installers/mongodb.sh
+++ apt-init/installers/mongodb.sh
@@ -5,13 +5,13 @@
 log "Adding MongoDB APT key:" && \
 apt-key adv \
   --keyserver hkp://keyserver.ubuntu.com:80 \
-  --recv 0C49F3730359A14518585931BC711F9BA15703C6 \
+  --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5 \
 && \
 
 log "Adding MongoDB APT source:" && \
 echo \
-  "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" \
-  | tee /etc/apt/sources.list.d/mongodb-org-3.4.list \
+  "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.6 multiverse" \
+  | tee /etc/apt/sources.list.d/mongodb-org-3.6.list \
 && \
 
 log "Updating APT:" && \
