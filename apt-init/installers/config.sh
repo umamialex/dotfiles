cd758c6dfce9e7507492521348bd0b64bbfd5ec7
Alexander Martin <alex@suitupalex.com>
2018-05-04 10:45:59 -0700

    config: Use symlinks to install configuration files.

--- apt-init/installers/config.sh
+++ apt-init/installers/config.sh
@@ -3,22 +3,22 @@
 . ./apt-init/log.sh && \
 
 log "Installing git config:" && \
-sudo -u $USER cp .gitconfig ~/.gitconfig && \
+sudo -u $USER ln -rs .gitconfig ~/.gitconfig && \
 
 log "Installing global gitignore:" && \
-sudo -u $USER cp .gitignore_global ~/.gitignore_global && \
+sudo -u $USER ln -rs .gitignore_global ~/.gitignore_global && \
 
 log "Installing .vimrc:" && \
-sudo -u $USER cp .vimrc ~/.vimrc && \
+sudo -u $USER ln -rs .vimrc ~/.vimrc && \
 
 log "Installing .eslintrc:" && \
-sudo -u $USER cp .eslintrc ~/.eslintrc && \
+sudo -u $USER ln -rs .eslintrc ~/.eslintrc && \
 
 log "Installing fish config directory:" && \
 sudo -u $USER mkdir -p ~/.config/fish && \
 
 log "Installing fish config:" && \
-sudo -u $USER cp config.fish ~/.config/fish/config.fish && \
+sudo -u $USER ln -rs config.fish ~/.config/fish/config.fish && \
 
 log "Installing vim bundles:" && \
 sudo -u $USER ./vim-bundle-init.sh && \
