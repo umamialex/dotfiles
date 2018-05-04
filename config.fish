1e68cad0549eb6315116e47c2ad22fa25438c73c
Alexander Martin <alex@suitupalex.com>
2018-05-04 10:39:40 -0700

    fish: Added tenfold shortcut.

--- config.fish
+++ config.fish
@@ -5,6 +5,7 @@ alias me="dev; and cd martin-experiments"
 alias p="dev; and cd personal"
 alias ctrl="me; and cd ctrl"
 alias agg="me; and cd athletes.gg"
+alias 10="dev; and cd tenfold"
 
 setenv SSH_ENV $HOME/.ssh/environment
 set PATH ~/.npm-global/bin $PATH
