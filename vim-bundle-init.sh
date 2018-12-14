8013a25698e62d951833106c6e0fc789ee66433e
Alexander Martin <alex@suitupalex.com>
2018-12-13 18:53:11 -0600

    vim: Clear vundle before cloning.

--- vim-bundle-init.sh
+++ vim-bundle-init.sh
@@ -5,6 +5,7 @@ echo "Initializing vim bundles..."
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
   curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim && \
   cd ~/.vim/bundle && \
+  rm -rf Vundle.vim && \
   git clone https://github.com/VundleVim/Vundle.vim.git && \
 
 echo "Done"
