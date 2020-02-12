3ab810090235f8b7f4d0c71104eeec592a59eb5a
Alexander Martin <alex@suitupalex.com>
2020-02-12 00:20:06 -0600

    fish: Update aliases and path.

--- config.fish
+++ config.fish
@@ -3,14 +3,16 @@ alias ls="ls -laX --color"
 alias dev="cd ~/Development"
 alias p="dev; and cd personal"
 alias agg="dev; and cd athletes.gg"
-alias ctrl="agg; and cd ctrl"
-alias tapi="tmux a -t agg-ms-api-v1"
+alias kordami="dev; and cd kordami"
+alias tenfold="dev; and cd tenfold"
+alias tapi="tmux a -t agg-ms-api"
+alias tapp="tmux a -t agg-app"
 
 setenv JAVA_HOME /mnt/c/Users/alex/Development/tenfold/jdk8
 setenv ANDROID_HOME /home/suitupalex/Android
 setenv SSH_ENV $HOME/.ssh/environment
 setenv DOCKER_HOST localhost:2375
-set PATH (yarn global bin) $JAVA_HOME/bin $PATH
+set PATH (yarn global bin) $JAVA_HOME/bin ~/.local/bin $PATH
 
 setenv NODE_ENV development
 setenv DEBUG "agg:*"
