9dd35b4cb0bb10e906e9c04ffc60d8dcb9e2a489
Alex Martin <alex@suitupalex.com>
2020-06-22 00:42:09 -0500

    node: Manual add NodeSource repository.

--- apt-init/prerequisites/node.sh
+++ apt-init/prerequisites/node.sh
@@ -2,5 +2,12 @@
 
 . ./apt-init/log.sh && \
 
-log "Downloading Node v14.x:" && \
-curl -sL https://deb.nodesource.com/setup_14.x | bash -
+VERSION=node_14.x && \
+DISTRO="$(lsb_release -s -c)" && \
+
+log "Add NodeSource signing key:" && \
+curl -sSL https://deb.nodesource.com/gpgkey/nodesource.gpg.key | sudo apt-key add - && \
+
+log "Add NodeSource repository:" && \
+echo "deb https://deb.nodesource.com/$VERSION $DISTRO main" | sudo tee /etc/apt/sources.list.d/nodesource.list && \
+echo "deb-src https://deb.nodesource.com/$VERSION $DISTRO main" | sudo tee -a /etc/apt/sources.list.d/nodesource.list
