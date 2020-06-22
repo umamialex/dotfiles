6a93ebbbd27ad4cbda0ee2a0ea6687868036d401
Alex Martin <alex@suitupalex.com>
2020-06-21 22:24:38 -0500

    tests: Added more commands.

--- tests/apt-init.sh
+++ tests/apt-init.sh
@@ -2,7 +2,20 @@
 
 node -v && \
 npm -v && \
+yarn -v &&\
+
+expo --version && \
+eslint --version && \
+feathers --version && \
+ngrok --version && \
+
+redis-cli --version && \
+redis-server --version && \
+mongod --version && \
 
 fish -v && \
-
+vim --version && \
+tmux -V && \
+ag --version && \
+htop --version && \
 git --version
