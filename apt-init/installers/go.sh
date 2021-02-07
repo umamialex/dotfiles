058af38198558046811adcfecaa67600122c24be
Alex Martin <alex@suitupalex.com>
2021-02-06 23:32:34 -0600

    go: Updated version and add package installs.

--- apt-init/installers/go.sh
+++ apt-init/installers/go.sh
@@ -2,9 +2,32 @@
 
 . ./apt-init/log.sh && \
 
-log "Downloading GoLang 1.14.4:" && \
-wget -O go.tar.gz "https://dl.google.com/go/go1.14.4.linux-amd64.tar.gz" && \
+log "Downloading GoLang 1.15.8:" && \
+wget -O go.tar.gz "https://golang.org/dl/go1.15.8.linux-amd64.tar.gz" && \
 
 log "Installing GoLang:" && \
 sudo tar -C /usr/local -xzf go.tar.gz && \
-rm go.tar.gz
+rm go.tar.gz && \
+
+log "Installing GoLang packages:" && \
+
+log "Installing tk:" && \
+fish -c "go get github.com/grafana/tanka/cmd/tk" && \
+fish -c "tk --version" && \
+
+log "Installing logcli:" && \
+fish -c "go get github.com/grafana/loki/cmd/logcli" && \
+fish -c "logcli --version" && \
+
+log "Installing jsonnet:" && \
+fish -c "go get github.com/google/go-jsonnet/cmd/jsonnet" && \
+fish -c "jsonnet --version" && \
+fish -c "jsonnetfmt --version" && \
+
+log "Installing jb:" && \
+fish -c "go get github.com/jsonnet-bundler/jsonnet-bundler/cmd/jb" && \
+fish -c "jb --version" && \
+
+log "Installing amtool:" && \
+fish -c "go get github.com/prometheus/alertmanager/cmd/amtool" && \
+fish -c "amtool --version"
