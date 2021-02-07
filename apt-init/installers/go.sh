13273a04b6cac0f3e7948c15563dfc184f1f8140
Alex Martin <alex@suitupalex.com>
2021-02-07 01:56:23 -0600

    go: Skip command installs on CI.

--- apt-init/installers/go.sh
+++ apt-init/installers/go.sh
@@ -11,22 +11,24 @@ rm go.tar.gz && \
 
 log "Installing GoLang packages:" && \
 
-log "Installing tk:" && \
-fish -c "go get github.com/grafana/tanka/cmd/tk" && \
-fish -c "tk --version" && \
+if [[ "$CI" != "true" ]]; then
+  log "Installing tk:" && \
+  fish -c "go get github.com/grafana/tanka/cmd/tk" && \
+  fish -c "tk --version" && \
 
-log "Installing logcli:" && \
-fish -c "go get github.com/grafana/loki/cmd/logcli" && \
-fish -c "logcli --version" && \
+  log "Installing logcli:" && \
+  fish -c "go get github.com/grafana/loki/cmd/logcli" && \
+  fish -c "logcli --version" && \
 
-log "Installing jsonnet:" && \
-fish -c "go get github.com/google/go-jsonnet/cmd/jsonnet" && \
-fish -c "jsonnet --version" && \
+  log "Installing jsonnet:" && \
+  fish -c "go get github.com/google/go-jsonnet/cmd/jsonnet" && \
+  fish -c "jsonnet --version" && \
 
-log "Installing jb:" && \
-fish -c "go get github.com/jsonnet-bundler/jsonnet-bundler/cmd/jb" && \
-fish -c "jb --version" && \
+  log "Installing jb:" && \
+  fish -c "go get github.com/jsonnet-bundler/jsonnet-bundler/cmd/jb" && \
+  fish -c "jb --version" && \
 
-log "Installing amtool:" && \
-fish -c "go get github.com/prometheus/alertmanager/cmd/amtool" && \
-fish -c "amtool --version"
+  log "Installing amtool:" && \
+  fish -c "go get github.com/prometheus/alertmanager/cmd/amtool" && \
+  fish -c "amtool --version"
+fi
