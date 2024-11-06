8b7ecc34f6a597b4c5dcb44fc215c24963807df5
Alex Martin <alex@suitupalex.com>
2024-11-06 10:05:09 -0800

    node: migrate installation

--- ubuntu/scripts/repositories/node.sh
+++ ubuntu/scripts/repositories/node.sh
@@ -2,12 +2,6 @@
 
 . ./ubuntu/scripts/log.sh && \
 
-VERSION=node_23.x && \
-DISTRO="$(lsb_release -s -c)" && \
-
-log "Add NodeSource signing key:" && \
-curl -sSL https://deb.nodesource.com/gpgkey/nodesource.gpg.key | sudo apt-key add - && \
-
 log "Add NodeSource repository:" && \
-echo "deb https://deb.nodesource.com/$VERSION $DISTRO main" | sudo tee /etc/apt/sources.list.d/nodesource.list && \
-echo "deb-src https://deb.nodesource.com/$VERSION $DISTRO main" | sudo tee -a /etc/apt/sources.list.d/nodesource.list
+curl -fsSL https://deb.nodesource.com/setup_23.x -o nodesource_setup.sh && \
+sudo -E bash nodesource_setup.sh
