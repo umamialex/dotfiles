ceecb66c389281a8f913a7667555f27cec730433
Alex Martin <alex@suitupalex.com>
2024-11-06 09:00:07 -0800

    git: clean up initial clones

--- ubuntu/scripts/installers/git.sh
+++ ubuntu/scripts/installers/git.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
 
-. ./ubuntu/scripts/git-clone.sh && \
-
-clone athletesgg/agg-app yarn
-clone athletesgg/agg-daemon yarn
-clone athletesgg/agg-ms-api yarn
-clone athletesgg/agg-sdk yarn
-
-clone grafana/backend-enterprise
-clone grafana/cortex-jsonnet
-clone grafana/customers
-clone grafana/deployment_tools
-clone grafana/helm-charts
-clone grafana/proserv-training-demo
-clone grafana/website
+. ./ubuntu/scripts/git-clone.sh
