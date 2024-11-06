aae5958e1ab4ab04834dda2c2d661921307e4384
Alex Martin <alex@suitupalex.com>
2024-11-06 09:12:26 -0800

    kubectl: migrate to community repo

--- ubuntu/scripts/repositories/kubectl.sh
+++ ubuntu/scripts/repositories/kubectl.sh
@@ -3,7 +3,7 @@
 . ./ubuntu/scripts/log.sh && \
 
 log "Adding kubectl signing key:" && \
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add - && \
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg && \
 
 log "Adding kubectl repo:" && \
-echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
