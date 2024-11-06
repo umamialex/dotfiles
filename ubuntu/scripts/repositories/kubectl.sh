31b37c1b445a6372db085e838ae223d10722c84c
Alex Martin <alex@suitupalex.com>
2024-11-06 10:04:50 -0800

    kubectl: auto yes installation

--- ubuntu/scripts/repositories/kubectl.sh
+++ ubuntu/scripts/repositories/kubectl.sh
@@ -3,7 +3,7 @@
 . ./ubuntu/scripts/log.sh && \
 
 log "Adding kubectl signing key:" && \
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg && \
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --always-trust --dearmor --batch --yes -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg && \
 
 log "Adding kubectl repo:" && \
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
