6ab80f8d05c17da94c8eb5589b57d652762895c3
Alex Martin <alex@suitupalex.com>
2024-11-06 10:04:35 -0800

    helm: migrate installation

--- ubuntu/scripts/repositories/helm.sh
+++ ubuntu/scripts/repositories/helm.sh
@@ -3,7 +3,8 @@
 . ./ubuntu/scripts/log.sh && \
 
 log "Add Helm signing key:" && \
-curl https://baltocdn.com/helm/signing.asc | sudo apt-key add - && \
+curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
+sudo apt-get install apt-transport-https --yes && \
 
 log "Add Helm repository:" && \
-echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
